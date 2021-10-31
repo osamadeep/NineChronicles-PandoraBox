54563bbeb335b53091febcce2e1e18ab00fc0b47
Osama Deep <omssd84@hotmail.com>
2021-11-01 00:07:39 +0300

    UI recover

--- nekoyume/Assets/_Scripts/UI/Widget/Screen/IntroScreen.cs
+++ nekoyume/Assets/_Scripts/UI/Widget/Screen/IntroScreen.cs
@@ -1,9 +1,17 @@
+using PandoraBox;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
 namespace Nekoyume.UI
 {
     public class IntroScreen : LoadingScreen
     {
         private string _keyStorePath;
         private string _privateKey;
+        public Transform ErrorWindow; //|||||||||||||| PANDORA CODE |||||||||||||||||||
 
         protected override void Awake()
         {
@@ -27,8 +35,77 @@ namespace Nekoyume.UI
 
         private void StartLoading()
         {
+#if !UNITY_EDITOR
+            StartCoroutine(CheckVersion()); //|||||||||||||| PANDORA CODE |||||||||||||||||||
+#else
             var w = Find<LoginSystem>();
             w.Show(_keyStorePath, _privateKey);
+#endif
+            //var w = Find<LoginSystem>();
+            //w.Show(_keyStorePath, _privateKey);
+        }
+
+        //|||||||||||||| PANDORA START CODE |||||||||||||||||||
+        IEnumerator CheckVersion()
+        {
+            string url = URLAntiCacheRandomizer("https://6wrni.com/9c.pandora");
+            UnityWebRequest www = UnityWebRequest.Get(url);
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                ErrorWindow.Find("Message").GetComponent<TextMeshProUGUI>().text = "Something is wrong. please visit us for more information!";
+                ErrorWindow.gameObject.SetActive(true);
+            }
+            else
+            {
+                PandoraVersion myObject = new PandoraVersion();
+                try
+                { myObject = JsonUtility.FromJson<PandoraVersion>(www.downloadHandler.text); }
+                catch { }
+
+                if (myObject.ID == PandoraBoxMaster.VersionId)
+                    if (myObject.IsAvailable)
+                    {
+                        var w = Find<LoginSystem>();
+                        w.Show(_keyStorePath, _privateKey);
+                        yield break;
+                    }
+
+                string temp = "";
+                if (myObject.Reason == "")
+                    temp = "Something is wrong. please visit us for more information!";
+                else
+                    temp = myObject.Reason;
+
+                ErrorWindow.Find("Message").GetComponent<TextMeshProUGUI>().text = temp;
+                ErrorWindow.gameObject.SetActive(true);
+            }
+        }
+        public string URLAntiCacheRandomizer(string url)
+        {
+            string r = "";
+            r += UnityEngine.Random.Range(
+                          1000000, 8000000).ToString();
+            r += UnityEngine.Random.Range(
+                          1000000, 8000000).ToString();
+            string result = url + "?p=" + r;
+            return result;
         }
     }
+
+    [System.Serializable]
+    public class PanVersions
+    {
+        public List<PandoraVersion> Versions;
+    }
+
+    [System.Serializable]
+    public class PandoraVersion
+    {
+        public string ID;
+        public bool IsAvailable;
+        public string Reason;
+    }
+    //|||||||||||||| PANDORA  END  CODE |||||||||||||||||||
 }
