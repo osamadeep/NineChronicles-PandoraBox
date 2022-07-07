182b60aeba8b7ecf9afb0c5dd07f4467f1392468
Osama Deep <omssd84@hotmail.com>
2022-07-07 12:02:55 +0300

    pre 57

--- nekoyume/Assets/_Scripts/PandoraBox/Scripts/PandoraBoxMaster.cs
+++ nekoyume/Assets/_Scripts/PandoraBox/Scripts/PandoraBoxMaster.cs
@@ -15,7 +15,7 @@ namespace Nekoyume.PandoraBox
 
         //Unsaved Reg Settings 
         public static string OriginalVersionId = "v100241";
-        public static string VersionId = "010056";
+        public static string VersionId = "010057A";
 
         //Pandora Database
         public static PanDatabase PanDatabase;
